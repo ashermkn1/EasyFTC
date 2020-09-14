ae0670a0efdf874ea31b0bc8d8a60d06863a328d
asher kornfeld <ashkornfeld@gmail.com>
2020-09-14 17:10:38 -0400

    changed turning mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test2WheelGamePad.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test2WheelGamePad.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.util.Range;
 
 @TeleOp
 public class Test2WheelGamePad extends Test2Wheel {
