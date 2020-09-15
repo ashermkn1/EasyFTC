2ae329a28f7d1422b570b71d1c72f8c1d69f86a3
asher kornfeld <ashkornfeld@gmail.com>
2020-09-15 18:17:26 -0400

    cleaned up VisionAuto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpModes/VisionAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpModes/VisionAuto.java
@@ -2,7 +2,9 @@ package org.firstinspires.ftc.teamcode.AutonomousOpModes;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+import org.firstinspires.ftc.robotcontroller.external.samples.ConceptTensorFlowObjectDetectionWebcam;
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
@@ -19,7 +21,8 @@ public class VisionAuto extends SuperOp {
 
     private static final int A = 0;
     private static final int B = 1;
-    private static final int C = 4;
+
+    //private static final int C = 4;
 
 
     private final static String VUFORIA_KEY = "ARgYuCf/////AAABmUYfc1+dVEQsgUBCPA2kCAFRmuTRB/XUfAJzLsRyFDRg6uMMjj6EXM8YNiY5l3oTw83H+PKgfF46gctdzrln2nnVXMebpgN9ULy1cOfdSsPk0hwSZqzcY0LWCj+rPPrZ3JyQT7gf2aw7bo8ZvWedWB7skuGIjg+9cyTJdDyXmXrQ8Bo4r4siTFNTVFxg21OH/Gd8wrVJF4RqjE+kcez3MzcnE2EPCqWTNixSge5yLg+tN87/R/dMPzqHWvmjE6F6J/7/sahPt7FQ9G6tYWnV1impzZsH7T/JT6pGr2SALwHdaNjBGbYY76ZfvAxixEdob9g6qMBhKOyLg6HTP9VzRZ06ksUhErmR2K2LSkyjxBBz";
@@ -28,28 +31,38 @@ public class VisionAuto extends SuperOp {
 
     private TFObjectDetector tfod;
 
-    private boolean seenRings = false;
     private int numRings;
     private AUTOSTATUS status = AUTOSTATUS.START;
 
     @Override
     public void init() {
         super.init();
+        // initialize vuforia
         initVuforia();
+        // initialize tfod
         if (ClassFactory.getInstance().canCreateTFObjectDetector()) {
             initTfod();
         } else {
             telemetry.addData("Sorry!", "This device is not compatible with TFOD");
         }
+        // add logging for motor powers of the drive train
         telemetry.addData("Drive Train: ", wheels);
+
+        // activate the object detection
+        if (tfod != null) {
+            tfod.activate();
+        }
     }
 
     @Override
     public void loop() {
-
         switch (status) {
             // detect rings
             case START:
+                if (tfod == null) {
+                    status = AUTOSTATUS.STOP;
+                    break;
+                }
                 List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
                 // make sure objects were detected
                 if (updatedRecognitions != null) {
@@ -61,24 +74,27 @@ public class VisionAuto extends SuperOp {
                 break;
             // move goal to defined zone
             case MOVEGOAL:
-                // drive to A
+                // drive from start position to A
 
                 if (numRings == A) {
                     // place wobble goal in zone
+
                     status = AUTOSTATUS.SHOOTRINGS;
                     break;
                 }
 
-                // drive to B
+                // drive from A to B
 
                 if (numRings == B) {
                     // rotate robot 180 degrees and place wobble goal in zone
+
                     status = AUTOSTATUS.SHOOTRINGS;
                     // rotate back 180 degrees
+
                     break;
                 }
 
-                // drive to C
+                // drive from B to C
 
                 // place wobble goal
 
@@ -91,13 +107,24 @@ public class VisionAuto extends SuperOp {
                 break;
 
             case PARK:
-                // drive so robot is over midline
+                // drive so robot is over midline, possibly using NavImages on the field
 
+                break;
+
+            case STOP:
+                // shutdown tfod object
+                if (tfod != null) {
+                    tfod.shutdown();
+                }
+                // stop all wheels
+                wheels.setPower(0, 0, 0);
         }
     }
 
 
-
+    /**
+     * Taken from {@link ConceptTensorFlowObjectDetectionWebcam}
+     */
 
     private void initVuforia() {
         /*
@@ -106,7 +133,7 @@ public class VisionAuto extends SuperOp {
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
 
         parameters.vuforiaLicenseKey = VUFORIA_KEY;
-        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        parameters.cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
 
         //  Instantiate the Vuforia engine
         vuforia = ClassFactory.getInstance().createVuforia(parameters);
@@ -114,11 +141,16 @@ public class VisionAuto extends SuperOp {
         // Loading trackables is not necessary for the TensorFlow Object Detection engine.
     }
 
+    /**
+     * Taken from {@link ConceptTensorFlowObjectDetectionWebcam}
+     */
     private void initTfod() {
         int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
                 "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+
         tfodParameters.minimumConfidence = 0.8f;
+
         tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
         tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT);
     }
